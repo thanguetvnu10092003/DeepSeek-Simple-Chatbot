3a2577eff3513848c26b3c3e78e5ab1125ff2ece
LeToanThang <toanthangvietduc@gmail.com>
2026-03-11 12:35:04 +0700

    fix(server): Add Cache-Control headers to index route to prevent HTML caching

--- server.py
+++ server.py
@@ -64,7 +64,11 @@ class ConversationCreate(BaseModel):
 
 @app.get("/")
 async def serve_index():
-    return FileResponse("static/index.html")
+    response = FileResponse("static/index.html")
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 
 @app.get("/api/files")
